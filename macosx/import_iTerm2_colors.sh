f676b9ab580e2056039b5549c9ebf8cc5ed2e4bf
Denilson Sá Maia <denilsonsa@gmail.com>
2024-05-29 10:31:46 +0200

    Comment about the source of the itermcolors file

--- macosx/import_iTerm2_colors.sh
+++ macosx/import_iTerm2_colors.sh
@@ -6,3 +6,7 @@ test -f ~/Library/Preferences/com.googlecode.iterm2.plist || exit
 grep -q Breath ~/Library/Preferences/com.googlecode.iterm2.plist && exit
 # Open this file, which will automatically import it into iTerm2.
 open macosx/Breath_Silverfox.itermcolors
+
+# This itermcolors file was generated from https://github.com/Gogh-Co/Gogh
+# By editing https://github.com/Gogh-Co/Gogh/blob/master/apply-colors.sh
+# removing the `rm` call from the `apply_darwin()` function.
