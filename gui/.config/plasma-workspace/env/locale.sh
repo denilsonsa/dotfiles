037cb3361f184de9f0f933aa337890c544fb5063
Denilson Sá Maia <denilsonsa@gmail.com>
2024-06-09 09:31:23 +0200

    KDE/Plasma 6 migration

--- gui/.config/plasma-workspace/env/locale.sh
+++ gui/.config/plasma-workspace/env/locale.sh
@@ -3,3 +3,6 @@
 # https://userbase.kde.org/Session_Environment_Variables
 
 export LANG=en_GB.UTF-8
+export LC_MEASUREMENT=nl_NL.UTF-8
+export LC_MONETARY=nl_NL.UTF-8
+export LC_TELEPHONE=nl_NL.UTF-8
