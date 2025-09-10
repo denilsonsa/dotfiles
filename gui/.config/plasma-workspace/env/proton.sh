2d8113a98fa21414c9a2108a0d3510071cd241d6
Denilson Sá Maia <denilsonsa@gmail.com>
2025-09-10 08:45:48 +0200

    Adding Proton Wine to PATH to make itch.io desktop client happy

---
+++ gui/.config/plasma-workspace/env/proton.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Having `wine` in $PATH causes the itch.io desktop client to allow downloading Windows-only games.
+# https://itch.io/t/694459/please-allow-installing-windows-versions-under-linux
+# https://itch.io/post/7913727
+
+PROTON="$HOME/.local/share/Steam/steamapps/common/Proton - Experimental/files/bin"
+if [ "$USER" = "deck" ] ; then
+	export PATH="$PATH:$PROTON"
+fi
